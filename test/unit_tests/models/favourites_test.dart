ca31472caa984d20a0274c8ba977ce246f0d2b1c
jay_wu <jay_wu>
2021-08-28 20:44:29 +0800

    [unit test] add unit test for favourite model

---
+++ test/unit_tests/models/favourites_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test_practice_2/models/favorites.dart';
+
+void main() {
+  group('Testing App Provider Tests', () {
+    var favorites = Favorites();
+
+    test('A new item should be added', () {
+      var number = 35;
+      favorites.add(number);
+      expect(favorites.items.contains(number), true);
+    });
+
+    test('An item should be removed', () {
+      var number = 45;
+      favorites.add(number);
+      expect(favorites.items.contains(number), true);
+      favorites.remove(number);
+      expect(favorites.items.contains(number), false);
+    });
+  });
+}
